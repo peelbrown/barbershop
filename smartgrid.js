33f78cc71a3e96d11b6c414b8bfeedb6cf641253
Phillip <peelbrown@gmail.com>
2020-01-11 14:01:38 -0500

    Started to track files. Changes were made on 1/10 at work

---
+++ smartgrid.js
@@ -0,0 +1,22 @@
+module.exports = {
+    columns: 12,
+    offset: '10px',
+    // offset: '3%',
+    // mobileFirst: true,
+    container: {
+        maxWidth: '1675px',
+        fields: '30px'
+    },
+    // breakPoints: {
+    //     md: {
+    //         width: '750px',
+    //         fields: '15px'
+    //     },	
+    //     sm: {
+    //         width: '720px'
+    //     },
+    //     xs: {
+    //         width: '576px'
+    //     }
+    // }
+}
